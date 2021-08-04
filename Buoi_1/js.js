c3830f43f19e38464871d597e27e6f9bfe8f7c20
NGOCANH198 <88376657+NGOCANH198@users.noreply.github.com>
2021-08-04 10:04:02 +0700

    Add files via upload

---
+++ Buoi_1/js.js
@@ -0,0 +1,23 @@
+/* Bai 1:
+let fullName = prompt('Hi there, your name please?');
+alert(`Hi ${fullName}`);      
+
+Bai 2: 
+let firstName = prompt('Enter your first name');
+let lastName = prompt('Enter your last name');
+alert(`Hi ${lastName} ${firstName}`);
+
+Bai 3:
+let lengthSquare = Number(prompt('Enter side length of the square'));
+alert(`The square area is  ${ lengthSquare*lengthSquare} `);
+       
+Bai4:
+let sizeCricle = Number(prompt('Enter the radius of the Circle'));
+const pi = 3.14;
+alert(`The Circle area is ${sizeCricle * sizeCricle * pi}`);
+
+Bai 5:
+*/
+let c = Number(prompt('Enter Celcius:'));
+let f = c * 1.8 +32;
+alert(` ${c} (C) = ${f}(F)`);
